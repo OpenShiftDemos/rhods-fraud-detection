48c0ce67187179108fc0533b1adf4fb5d44dab3a
Bob Kozdemba <bkozdemba@gmail.com>
2022-04-18 09:40:51 -0500

    added metrics

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,9 +1,14 @@
 import json
 from flask import Flask, jsonify, request
 from prediction import predict
+from prometheus_client import Counter
+from prometheus_client import start_http_server
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
 
 application = Flask(__name__)
 
+c = Counter('visit_count', 'Visit counter')
 
 @application.route('/')
 @application.route('/status')
@@ -13,6 +18,12 @@ def status():
 
 @application.route('/predictions', methods=['POST'])
 def create_prediction():
+    c.inc()
     data = request.data or '{}'
     body = json.loads(data)
     return jsonify(predict(body))
+
+# Add prometheus wsgi middleware to route /metrics requests
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
