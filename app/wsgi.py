fd19cd3a9569b1405ec7844b2e1c9eabc6bfcae6
Bob Kozdemba <bkozdemba@gmail.com>
2022-04-18 16:03:02 -0500

    added additional metrics

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,14 +1,20 @@
 import json
+from pydoc import pager
 from flask import Flask, jsonify, request
 from prediction import predict
 from prometheus_client import Counter
 from prometheus_client import start_http_server
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from prometheus_client import make_wsgi_app
+import logging
 
 application = Flask(__name__)
 
 c = Counter('requests_count', 'Requests counter')
+legit = Counter('legit_count', 'Legitimate counter')
+fraud = Counter('fraud_count', 'Fraud counter')
+
+logging.basicConfig(level=logging.INFO)
 
 @application.route('/')
 @application.route('/status')
@@ -21,7 +27,20 @@ def create_prediction():
     c.inc()
     data = request.data or '{}'
     body = json.loads(data)
-    return jsonify(predict(body))
+    p = predict(body)
+    
+    #
+    # Metrics
+    #
+    if p["prediction"] == "legitimate":
+            legit.inc()
+    if p["prediction"] == "fraud":
+            fraud.inc()
+
+    logging.debug(f'Prediction: {p["prediction"]}')
+    
+    r = jsonify(p)
+    return r
 
 # Add prometheus wsgi middleware to route /metrics requests
 application.wsgi_app = DispatcherMiddleware(application.wsgi_app, {
